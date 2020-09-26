6b35911d2ac68e0c30fa1d75439587a637f3cbe4
Ekaterina <ekaterinan112@gmail.com>
2020-09-26 17:23:31 +0300

    Updates

---
+++ precache-manifest.e77dd7c62ada1ac8912d33c7b2329ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ca84981459594c4ab2bf85cec256e4",
+    "url": "/IgnatTasksRep/index.html"
+  },
+  {
+    "revision": "99e3aaaef3ac0b71aa95",
+    "url": "/IgnatTasksRep/static/css/main.6e8be272.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/IgnatTasksRep/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IgnatTasksRep/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e3aaaef3ac0b71aa95",
+    "url": "/IgnatTasksRep/static/js/main.8ed88b1a.chunk.js"
+  },
+  {
+    "revision": "f00df8e44ae2e46fddc9",
+    "url": "/IgnatTasksRep/static/js/runtime-main.6f2dcb3c.js"
+  }
+]);
