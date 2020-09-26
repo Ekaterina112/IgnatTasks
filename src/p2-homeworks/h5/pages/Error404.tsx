07aac6cbe61bb52aa4c7f9f1e5083481d69cb16f
Ekaterina <ekaterinan112@gmail.com>
2020-09-26 20:30:45 +0300

    HW5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -8,7 +8,7 @@ import {PATH} from '../Routes';
 function Error404() {
     return (
         <div>
-            <img src="https://pixel-one.ru/uploads/posts/2017-10/1508524003_grumpy-cat.jpg"  alt="here is cat" className={s.bg}/>
+            <img src="https://pixel-one.ru/uploads/posts/2017-10/1508524003_grumpy-cat.jpg"  alt="here is a cat" className={s.bg}/>
             <div className={s.homepage}> Please, return at
                 <span>
                     <NavLink to={PATH.PRE_JUNIOR}> HOMEPAGE </NavLink>
